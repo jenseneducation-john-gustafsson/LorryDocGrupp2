afac86e36c5573f424cd8709374efe8968e4a05e
jenseneducation-john-gustafsson <john.gustafsson80@gmail.com>
2021-02-10 16:52:24 +0100

    första commit

---
+++ client/src/App.js
@@ -0,0 +1,192 @@
+import './App.css';
+import Axios from "axios";
+import React from "react";
+
+function App() {
+
+  const userInput = [{ brand: "", issue: "", diagnostics: "", remedy: "" }];
+
+  class Brands extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        change: false,
+        brands: [],
+      }
+    }
+
+    componentDidMount() {
+      Axios.get("http://localhost:3001/brand").then((response) => {
+        this.setState({ brands: response.data });
+      });
+    }
+
+    selectBrand = (event) => {
+      console.log(event.target.value)
+      this.setState({ change: true });
+    };
+
+    render() {
+      return (
+        <>
+          <div className="App">
+            <div className="brands">
+              <select id="cars" onChange={this.selectBrand}>
+                <option value="märke">Välj Märke</option>
+                {this.state.brands.map((val, key) => {
+                  return <option id="option" key={key} value={val.Brand}>{val.Brand}</option>
+                })}
+              </select>
+            </div>
+            {this.state.change ? <Issue /> : null}
+          </div>
+        </>
+      )
+    }
+  }
+
+  class Issue extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        change: false,
+        issues: [],
+      }
+    }
+
+    componentDidMount() {
+      Axios.get("http://localhost:3001/issue/").then((response) => {
+        this.setState({ issues: response.data });
+      });
+    }
+
+
+    selectBrand = (event) => {
+      userInput['issue'] = event.target.value;
+      console.log(event.target.value)
+      this.setState({ change: true });
+    };
+
+
+
+    render() {
+      return (
+        <>
+          <div className="App">
+            <div className="issues">
+            <label>Problem: </label>
+              <select id="issues" onChange={this.selectBrand}>
+                <option value="issues">Problem</option>
+                {this.state.issues.map((val, key) => {
+                  return <option key={key} value={val.Issue}>{val.Issue}</option>
+                })}
+              </select>
+            </div>
+            {this.state.change ? <Diagnostics /> : null}
+          </div>
+        </>
+      )
+    }
+  }
+
+  class Diagnostics extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        change: false,
+        diagnostics: [],
+      }
+    }
+
+    componentDidMount() {
+      Axios.get(`http://localhost:3001/diagnostics/${userInput.issue}`).then((response) => {
+        this.setState({ diagnostics: response.data });
+      });
+    }
+
+    selectBrand = (event) => {
+      userInput['diagnostics'] = event.target.value;
+      console.log(event.target.value);
+      this.setState({ change: true });
+    };
+
+    //Diganostic regex filter
+    replaceRegex = (value) => {
+      let reg = new RegExp(`\\d\\W\\S\\b${userInput.issue.toLowerCase()}\\b\\W\\W`);
+      let newValue = value.replace(reg,'');
+      return newValue
+    }
+
+    render() {
+      return (
+        <>
+          <div className="App">
+            <div className="diagnosticsues">
+            <label>Diagnos: </label>
+              <select id="diagnostics" onChange={this.selectBrand}>
+                <option value="diagnostics">Diagnos</option>
+                {this.state.diagnostics.map((val, key) => {
+                  return <option key={key} value={this.replaceRegex(val.diagnostics)}>{this.replaceRegex(val.diagnostics)}</option>
+                })}
+              </select>
+            </div>
+            {this.state.change ? <Remedy /> : null}
+          </div>
+        </>
+      )
+    }
+  }
+
+  class Remedy extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        change: false,
+        remedy: [],
+      }
+    }
+
+    componentDidMount() {
+      Axios.get(`http://localhost:3001/remedy/${userInput.diagnostics}`).then((response) => {
+        this.setState({ remedy: response.data });
+      });
+    }
+
+    selectBrand = (event) => {
+      console.log(event.target.value)
+      this.setState({ change: true });
+    };
+    //Remedy regex filter
+    replaceRegex = (value) => {
+      let reg = new RegExp(`\\d\\d\\W\\S\\b${userInput.issue.toLowerCase()}\\b\\W\\W`);
+      let newValue = value.replace(reg,'');
+      return newValue
+    }
+
+    clearList(){};
+
+    render() {
+      return (
+        <>
+          <form onSubmit={this.clearList}>
+          <div className="App">
+            <div className="remedy">
+              <p>Lösning</p>
+              {this.state.remedy.map((val, key) => {
+                return <h3 key={key} value={this.replaceRegex(val.remedy)}>{this.replaceRegex(val.remedy)}</h3>
+              })}
+              <button class="btn" type='submit'>Submit</button>
+            </div>
+          </div>
+        </form>
+        </>
+      )
+    }
+  }
+  return (
+    <>
+      <Brands />
+    </>
+  )
+}
+export default App;
---
+++ server/index.js
@@ -0,0 +1,189 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "john",
+  password: "1234",
+  database: "lorrydoc"
+});
+
+//MYSQL SELECT
+const brands = "SELECT Brand FROM allcolumn WHERE brand IS NOT NULL";
+const issues = "SELECT Issue FROM allcolumn WHERE issue IS NOT NULL";
+
+
+
+app.get("/brand", (req, res) => {
+  db.query(brands, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.get("/issue", (req, res) => {
+  db.query(issues, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+})
+
+app.get("/diagnostics/:issue", (req, res) => {
+
+  switch (req.params.issue) {
+    case 'Motor':
+      db.query("SELECT diagnostics FROM AllColumn where diagnostics REGEXP 'motor'", (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      });
+      break;
+
+    case 'Hjul':
+      issue = 'hjul';
+      db.query("SELECT diagnostics FROM AllColumn where diagnostics REGEXP 'hjul'", (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      });
+      break;
+
+    case 'Lampor':
+      issue = 'lampor';
+      db.query("SELECT diagnostics FROM AllColumn where diagnostics REGEXP 'lampor'", (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      });
+      break;
+
+    case 'Bromsar':
+      issue = 'bromsar';
+      db.query("SELECT diagnostics FROM AllColumn where diagnostics REGEXP 'bromsar'", (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      });
+      break;
+  };
+});
+
+app.get("/remedy/:diagnostics", (req, res) => {
+
+  switch (req.params.diagnostics) {
+    case 'Motorlampa tänd':
+      db.query("SELECT remedy FROM AllColumn where remedy REGEXP '11'", (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      });
+      break;
+
+    case 'Lastbilen startar inte':
+      db.query("SELECT remedy FROM AllColumn where remedy BETWEEN 21 AND 22", (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      });
+      break;
+
+    case 'Överhettad':
+      db.query("SELECT remedy FROM AllColumn where remedy REGEXP '31'", (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      });
+      break;
+
+    case 'Punktering':
+      db.query("SELECT remedy FROM AllColumn where remedy REGEXP '41'", (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      });
+      break;
+
+    case 'Lite luft':
+      db.query("SELECT remedy FROM AllColumn where remedy REGEXP '51'", (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      });
+      break;
+
+    case 'Hjul sitter löst (vibrerar)':
+      db.query("SELECT remedy FROM AllColumn where remedy REGEXP '61'", (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      });
+      break;
+
+    case 'Trasig lampa':
+      db.query("SELECT remedy FROM AllColumn where remedy REGEXP '71'", (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      });
+      break;
+
+    case 'Tar dåligt':
+      db.query("SELECT remedy FROM AllColumn where remedy REGEXP '81'", (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      });
+      break;
+
+    case 'Bromslampa lyser':
+      db.query("SELECT remedy FROM AllColumn where remedy REGEXP '91'", (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      });
+      break;
+  };
+});
+
+app.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
